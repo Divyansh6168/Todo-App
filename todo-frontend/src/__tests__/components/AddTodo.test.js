c9ae04e63dc57cb545ce5dd190826e4084c32cd6
Divyansh6168 <divyanshrawat113@gmail.com>
2026-06-23 15:50:36 +0530

    Updated unit test for frontend

--- todo-frontend/src/__tests__/components/AddTodo.test.js
+++ todo-frontend/src/__tests__/components/AddTodo.test.js
@@ -25,6 +25,59 @@ describe("Testing the Add Todo component", () => {
         ).toBeInTheDocument();
     });
 
+    test("When form is submitted, the onAdd function is invoked", async () => {
+        const mockOnAdd = jest.fn().mockResolvedValue();
 
+        render(<AddTodo onAdd={mockOnAdd} />);
+
+        const input = screen.getByPlaceholderText(
+            "Enter a todo..."
+        );
+
+        const button = screen.getByRole("button", {
+            name: /Add Todo/i
+        });
+
+        fireEvent.change(input, {
+            target: {
+                value: "New Todo"
+            }
+        });
+
+        fireEvent.click(button);
+
+        await waitFor(() => {
+            expect(mockOnAdd)
+                .toHaveBeenCalledWith("New Todo");
+        });
+    });
+
+    test("Input gets cleared after submission", async () => {
+        const mockOnAdd = jest.fn().mockResolvedValue();
+
+        render(<AddTodo onAdd={mockOnAdd} />);
+
+        const input = screen.getByPlaceholderText(
+            "Enter a todo..."
+        );
+
+        const button = screen.getByRole("button", {
+            name: /Add Todo/i
+        });
+
+        fireEvent.change(input, {
+            target: {
+                value: "New Todo"
+            }
+        });
+
+        expect(input.value).toBe("New Todo");
+
+        fireEvent.click(button);
+
+        await waitFor(() => {
+            expect(input.value).toBe("");
+        });
+    });
 
 });
